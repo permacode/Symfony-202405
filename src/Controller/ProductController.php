b14edd7fc8ae2e55d2c9770f20054289b8a4c1d1
Miguel Segura <miguel.segura@outlook.fr>
2024-05-13 15:19:53 +0000

    Add entities and migrations

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -10,86 +11,60 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class ProductController extends AbstractController
 {
+    static array $products = [
+        [
+            'id' => 1,
+            'name' => 'Iphone 13',
+            'price' => 999.99,
+            'description' => 'Apple iPhone 13 Pro 256 Go Bleu Pacifique',
+            'category' => 'smartphone',
+        ],
+        [
+            'id' => 2,
+            'name' => 'Samsung Galaxy S21',
+            'price' => 899.99,
+            'description' => 'Samsung Galaxy S21 5G 128 Go Double SIM Noir Phantom',
+            'category' => 'smartphone',
+        ],
+        [
+            'id' => 3,
+            'name' => 'Huawei P40',
+            'price' => 799.99,
+            'description' => 'Huawei P40 Pro 5G 256 Go Double SIM Noir',
+            'category' => 'smartphone',
+        ],
+        [
+            'id' => 4,
+            'name' => 'MacBook Pro',
+            'price' => 1299.99,
+            'description' => 'Apple MacBook Pro 13" 256 Go SSD 8 Go RAM Intel Core i5 quadricœur à 1,4 GHz Argent',
+            'category' => 'laptop',
+        ]
+    ];
     #[Route('/products', name: 'app_product')]
     public function displayAllProducts(): JsonResponse
     {
-        $products = [
-            [
-                'id' => 1,
-                'name' => 'Iphone 13',
-                'price' => 999.99,
-                'description' => 'Apple iPhone 13 Pro 256 Go Bleu Pacifique',
-                'category' => 'smartphone',
-            ],
-            [
-                'id' => 2,
-                'name' => 'Samsung Galaxy S21',
-                'price' => 899.99,
-                'description' => 'Samsung Galaxy S21 5G 128 Go Double SIM Noir Phantom',
-                'category' => 'smartphone',
-            ],
-            [
-                'id' => 3,
-                'name' => 'Huawei P40',
-                'price' => 799.99,
-                'description' => 'Huawei P40 Pro 5G 256 Go Double SIM Noir',
-                'category' => 'smartphone',
-            ],
-            [
-                'id' => 4,
-                'name' => 'MacBook Pro',
-                'price' => 1299.99,
-                'description' => 'Apple MacBook Pro 13" 256 Go SSD 8 Go RAM Intel Core i5 quadricœur à 1,4 GHz Argent',
-                'category' => 'laptop',
-            ]
-        ];
-        return $this->json($products);
+        return $this->json(self::$products);
     }
 
-    #[Route('/product/{id}', name:"product_by_id")]
-    public function displayProductById(int $id):JsonResponse
+    #[Route('/product/{id}', name: "product_by_id")]
+    public function displayProductById(int $id): JsonResponse
     {
-        $products = [
-            [
-                'id' => 1,
-                'name' => 'Iphone 13',
-                'price' => 999.99,
-                'description' => 'Apple iPhone 13 Pro 256 Go Bleu Pacifique',
-                'category' => 'smartphone',
-            ],
-            [
-                'id' => 2,
-                'name' => 'Samsung Galaxy S21',
-                'price' => 899.99,
-                'description' => 'Samsung Galaxy S21 5G 128 Go Double SIM Noir Phantom',
-                'category' => 'smartphone',
-            ],
-            [
-                'id' => 3,
-                'name' => 'Huawei P40',
-                'price' => 799.99,
-                'description' => 'Huawei P40 Pro 5G 256 Go Double SIM Noir',
-                'category' => 'smartphone',
-            ],
-            [
-                'id' => 4,
-                'name' => 'MacBook Pro',
-                'price' => 1299.99,
-                'description' => 'Apple MacBook Pro 13" 256 Go SSD 8 Go RAM Intel Core i5 quadricœur à 1,4 GHz Argent',
-                'category' => 'laptop',
-            ]
-        ];
-        $returnedProduct = [];
-        if(!!$id){
-            foreach ($products as $product) {
-                if($product['id'] === $id) {
-                    $returnedProduct = $product;
-                }
-            }
+        // $returnedProduct = [];
+        // if (!!$id) {
+        //     foreach (self::$products as $product) {
+        //         if ($product['id'] === $id) {
+        //             $returnedProduct = $product;
+        //         }
+        //     }
 
-            return $this->json($returnedProduct);
-        } else {
-            return $this->json("Cette option  n'est pas disponible.");
-        }
+        //     return $this->json($returnedProduct);
+        // } else {
+        //     return $this->json("Cette option  n'est pas disponible.");
+        // }
+        return $this->json(array_filter(self::$products, fn ($product): bool => $id === $product['id']));
+        $data = (new ArrayCollection(self::$products))
+            ->findFirst(fn(int $key, array $product):bool =>
+                $id === $product['id']);
     }
 }
