d54ce6bb134e4150bc6e5689d3ecc426e689c078
Miguel Segura <miguel.segura@outlook.fr>
2024-05-14 10:43:01 +0000

    personnalizing admin controllers

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -5,9 +5,10 @@ namespace App\Entity;
 use App\Repository\CommentRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Stringable;
 
 #[ORM\Entity(repositoryClass: CommentRepository::class)]
-class Comment
+class Comment implements Stringable
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -108,5 +109,10 @@ class Comment
         $this->photoFilename = $photoFilename;
 
         return $this;
+    }
+
+    public function __toString(): string
+    {
+        return (string) $this->getEmail();
     }
 }
--- src/Entity/Conference.php
+++ src/Entity/Conference.php
@@ -6,9 +6,10 @@ use App\Repository\ConferenceRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Stringable;
 
 #[ORM\Entity(repositoryClass: ConferenceRepository::class)]
-class Conference
+class Conference implements Stringable
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -104,5 +105,10 @@ class Conference
         }
 
         return $this;
+    }
+
+    public function __toString(): string
+    {
+        return $this->city . ' ' . $this->year;
     }
 }
