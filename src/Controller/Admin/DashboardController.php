d80da4442fbb37ae9b73bb361a500fb3f6fed62f
Miguel Segura <miguel.segura@outlook.fr>
2024-05-14 12:07:17 +0000

    adding AdminCrudControllers for product and category and personnalizing of productController

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Category;
 use App\Entity\Comment;
 use App\Entity\Conference;
+use App\Entity\Product;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -17,8 +19,9 @@ class DashboardController extends AbstractDashboardController
     public function index(): Response
     {
         $routeBuilder = $this->container->get(AdminUrlGenerator::class);
-        $url = $routeBuilder->setController(ConferenceCrudController::class)->generateUrl();
+        $url = $routeBuilder->setController(ProductCrudController::class)->generateUrl();
         return $this->redirect($url);
+        // return parent::index();
     }
 
     public function configureDashboard(): Dashboard
@@ -32,5 +35,7 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linktoRoute('Back to the website', 'fas fa-home', 'homepage');
         yield MenuItem::linkToCrud('Conferences', 'fas fa-map-marker-alt', Conference::class);
         yield MenuItem::linkToCrud('Comments', 'fas fa-comments', Comment::class);
+        yield MenuItem::linkToCrud('Categories', 'fas fa-lsit', Category::class);
+        yield MenuItem::linkToCrud('Products', 'fas fa-lsit', Product::class);
     }
 }
---
+++ src/Controller/Admin/ProductCrudController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use Doctrine\DBAL\Types\FloatType;
+use Doctrine\DBAL\Types\TextType;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ProductCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Product::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud->
+            setEntityLabelInPlural('Products')
+        ;
+    }
+    
+    public function configureFields(string $pageName): iterable
+    {
+        // return [
+        //     IdField::new('id'),
+        //     TextField::new('title'),
+        //     TextEditorField::new('description'),
+        // ];
+        yield AssociationField::new('category');
+        yield TextField::new('name');
+        yield NumberField::new('price');
+        yield TextareaField::new('description');
+    }
+}
