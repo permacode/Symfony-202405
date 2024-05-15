97b37d145e28548eb98dbd983477e3fde4522dac
Miguel Segura <miguel.segura@outlook.fr>
2024-05-15 09:52:15 +0000

    deleting wrong entry in last commit

--- src/EventSubscriber/TwigEventSubscriber.php
+++ src/EventSubscriber/TwigEventSubscriber.php
@@ -13,14 +13,12 @@ class TwigEventSubscriber implements EventSubscriberInterface
     public function __construct(
         private Environment $twig,
         private ConferenceRepository $conferenceRepository,
-        private DashboardController $dashboard
     ) {
     }
 
     public function onControllerEvent(ControllerEvent $event): void
     {
         $this->twig->addGlobal('conferences', $this->conferenceRepository->findAll());
-        $this->twig->addGlobal('admin_link', $this->dashboard->index());
     }
 
     public static function getSubscribedEvents(): array
