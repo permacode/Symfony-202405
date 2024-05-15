d5215f77a9e92830e3b6dc49da0fefc80305827c
Miguel Segura <miguel.segura@outlook.fr>
2024-05-15 09:50:09 +0000

    Overriding ConferenceRepository findAll method

--- src/EventSubscriber/TwigEventSubscriber.php
+++ src/EventSubscriber/TwigEventSubscriber.php
@@ -2,6 +2,7 @@
 
 namespace App\EventSubscriber;
 
+use App\Controller\Admin\DashboardController;
 use App\Repository\ConferenceRepository;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
@@ -11,13 +12,15 @@ class TwigEventSubscriber implements EventSubscriberInterface
 {
     public function __construct(
         private Environment $twig,
-        private ConferenceRepository $conferenceRepository
+        private ConferenceRepository $conferenceRepository,
+        private DashboardController $dashboard
     ) {
     }
 
     public function onControllerEvent(ControllerEvent $event): void
     {
         $this->twig->addGlobal('conferences', $this->conferenceRepository->findAll());
+        $this->twig->addGlobal('admin_link', $this->dashboard->index());
     }
 
     public static function getSubscribedEvents(): array
