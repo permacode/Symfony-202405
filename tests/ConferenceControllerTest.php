91fb0a85d091ab7a911a88b374411f2467271e86
permacode y <ryudu26@hotmail.fr>
2024-06-04 12:55:15 +0000

    adding functionnal tests

---
+++ tests/ConferenceControllerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ConferenceControllerTest extends WebTestCase
+{
+    public function testIndex(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/conference');
+
+        static::assertResponseIsSuccessful();
+        static::assertSelectorTextContains('h2', 'Give your feedback!');
+    }
+
+    public function testConferencePage(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/conference');
+
+        static::assertCount(2, $crawler->filter('h4'));
+
+        $client->clickLink('View');
+
+        static::assertPageTitleContains('Amsterdam');
+
+        static::assertResponseIsSuccessful();
+        static::assertSelectorTextContains('h2', 'Amsterdam 2019');
+
+        static::assertSelectorExists('div:contains("There are 3 comment")');
+    }
+
+    public function testCommentSubmission(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/conference/amsterdam-2019');
+        $client->submitForm('Submit', [
+            'comment_form[author]' => 'Fabien',
+            'comment_form[text]' => 'Some feedback from an automated functional test',
+            'comment_form[email]' => 'me@automat.ed',
+            'comment_form[photo]' => dirname(__DIR__, 2) . '/public/images/under-construction.gif',
+        ]);
+        static::assertResponseRedirects();
+        $client->followRedirect();
+        static::assertSelectorExists('div:contains("There are 4 comments")');
+    }
+}
